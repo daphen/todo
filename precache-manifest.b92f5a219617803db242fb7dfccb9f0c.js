b6b9a0a5e4274a6234545fe0eb13cea9598133e5
David Karlsson <david.karlsson@generanetworks.com>
2019-03-14 15:32:23 +0100

    deploy

---
+++ precache-manifest.b92f5a219617803db242fb7dfccb9f0c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/todo/robots.txt"
+  },
+  {
+    "revision": "3d1a569ff02928955de5",
+    "url": "/todo/js/chunk-vendors.8a33bc67.js"
+  },
+  {
+    "revision": "5ac49b162650f3770e1a",
+    "url": "/todo/js/app.b534e7d0.js"
+  },
+  {
+    "revision": "1ff3a6f10c76ed4f28a938caa4006a70",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "5ac49b162650f3770e1a",
+    "url": "/todo/css/app.1083df92.css"
+  }
+];
